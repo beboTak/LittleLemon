4868926f1d6ef1411e3b359d4a93b742f2711541
ibrahim mosaad <ibrahimhamadamosaad@gmail.com>
2023-12-31 14:02:51 +0200

    second change

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Resaurant',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -53,18 +53,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'LittleLemon.urls'
 
 TEMPLATES = [
+
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
+                'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-        },
+                ],
+               },
     },
 ]
 
