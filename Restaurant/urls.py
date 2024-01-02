806f48920f2ac42258a455f07523a19ca9a739e6
ibrahim mosaad <ibrahimhamadamosaad@gmail.com>
2024-01-02 13:10:53 +0200

    get rest_framework

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -2,4 +2,6 @@ from . import views
 from django.urls import path
 urlpatterns = [
 path('index/', views.index, name='index'),
+path('menu/', views.MenuItemsView.as_view()),
+path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,6 +1,28 @@
+
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import menuSerializer,bookingSerializer
+from rest_framework.decorators import api_view
+from .models import Menu
 
-# Create your views here.
 def index(request):
-
  return render(request, 'index.html', {"":""})
+
+# def post(self,request):
+#   serializer = menuSerializer(data=request.data)
+
+#   if serializer.is_valid():
+#     serializer.save()
+
+#     return Response({"status":"success","data":serializer.data})
+  
+
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+ queryset = Menu.objects.all()
+ serializer_class = menuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = menuSerializer
